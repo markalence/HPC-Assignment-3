9d50ad0d9459ae5f5ed7827da2eab7a60a63536e
Mark Alence <mark.alence@switch.org.za>
2020-05-23 23:29:16 +0200

    i repeat

---
+++ cmake-build-debug-remote-host/CMakeFiles/hpc3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hpc3.dir/main.cu.o"
+  "CMakeFiles/hpc3.dir/cmake_device_link.o"
+  "hpc3.pdb"
+  "hpc3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/hpc3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
